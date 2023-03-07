39fd3100209a6108ea9531150d2949ddd25c4141
Ben Groff <44035402+BenGroff@users.noreply.github.com>
2023-03-07 12:30:10 -0700

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -1,3 +1,19 @@
+//runs when html is ready
+$(document).ready(async function(e) {
+
+    $(".back-to-top").click(function(e) {
+        // window.scrollTo(0, 0);
+        window.scroll({top: 0,  left: 0, behavior: 'smooth' });
+    });
+
+    //sets the footer submit button length
+    let width = 16;
+    let inputs = await $(".footer-form .inputs").children();
+    width += $(inputs[0]).width();
+    width += $(inputs[1]).width();
+    $(".footer-submit").css("width", `${width}px`)
+});
+
 //on scroll listener
 $(document).scroll(function(e) {
 
